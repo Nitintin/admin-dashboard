f15279ddbed912e4b92d45bc1a6727650a6f9d69
nitin gupta <ngupta5@expediagroup.com>
2024-03-04 09:30:12 +0530

    dynamically call widget component

--- src/components/WidgetWrapper.js
+++ src/components/WidgetWrapper.js
@@ -1,9 +1,45 @@
 import react from 'react';
 import Widget from './Widget';
 
-const WidgetWrapper = () =>{
-    return <nav className='widgetWrapper widgetGridContainer'><Widget/><Widget/><Widget/><Widget/></nav>
-}
+const WidgetWrapper = () => {
+    const products = [
+        {
+            name: 'Iphone',
+            category: 'electronic',
+            status: 'instock'
+        },
+        {
+            name: 'Book',
+            category: 'education',
+            status: 'instock'
+        },
+        {
+            name: 'Tshirt',
+            category: 'clothing',
+            status: 'instock'
+        },
+        {
+            name: 'microwave',
+            category: 'electronic',
+            status: 'outofstock'
+        }
+    ];
 
+    const getWidget = () => {
+        const arr = [];
+
+        for(let i=0; i<40; i++){
+            arr.push(<Widget />)
+        }
+
+        return arr;
+    }
+
+    return <p className='widgetWrapper widgetGridContainer'>
+        {
+            getWidget()
+        }
+    </p>
+}
 
 export default WidgetWrapper;
