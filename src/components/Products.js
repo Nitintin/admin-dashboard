7e870cb958c8139d889b0b6c632db3328c999a88
nitin gupta <ngupta5@expediagroup.com>
2024-03-17 22:20:30 +0530

    search functionality of SERP

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,10 +4,16 @@ import axios from 'axios';
 import DataTable from 'react-data-table-component';
 import Header from './Header';
 import Footer from './Footer';
+import { useNavigate } from 'react-router-dom';
 
 const Products = () => {
 
     const [productsData, setProductsData] = useState([]);
+    const [userSearchInput, setUserSearchInput] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
+    const [sugeestionList, setSuggestionList] = useState([]);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchProducts = async () => {
@@ -31,9 +37,49 @@ const Products = () => {
             sortable: true,
         },
     ];
+
+    const handleSearch = () => {
+        if(!userSearchInput){
+            return;
+        }
+
+        const filteredArray = productsData.filter(item => item.title.toLowerCase() === userSearchInput.toLowerCase());
+        console.log(filteredArray);
+        if(filteredArray.length > 0){
+            //happy scenario
+            navigate(`/products/${filteredArray[0].id}`)
+        }else{
+            //negative scenario
+            setErrorMsg('We do not have this product');
+        }
+    }
+
+    const handleInputChange = (e) => {
+        const userData = e.target.value;
+        setUserSearchInput(userData);
+
+        const filteredData = productsData.filter(item => item.title.toLowerCase().includes(userData.toLowerCase()));
+        setSuggestionList(filteredData);
+
+        if(!userData){
+            setErrorMsg('');
+        }
+    }
+
     return <>
         <Header />
             <h2>Product Page </h2>
+            <input placeholder='Search for product Name' value={userSearchInput} onChange={handleInputChange} />
+            <button type='button' onClick={handleSearch}>Search</button>
+            {
+                errorMsg ? <div>{errorMsg}</div> : null
+            }
+            <ul>
+                {
+                    sugeestionList.map(item => <Link to={`/products/${item.id}`}><li>{item.title}</li></Link>)
+                }
+            </ul>
+            <hr />
             <table>
                 <tr>
                     <td>Sno</td>
@@ -58,11 +104,11 @@ const Products = () => {
                     ))
                 }
             </table>
-            <DataTable
+            {/* <DataTable
                 columns={columns}
                 data={productsData}
                 selectableRows
-            />
+            /> */}
         <Footer />
     </>
 }
